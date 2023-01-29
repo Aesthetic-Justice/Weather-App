3a76388f7ffbc6b9fc23b202ea93865f43d06af6
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-01-29 18:39:14 -0500

    sidebar now interactable

--- js/weatherscript.js
+++ js/weatherscript.js
@@ -2,6 +2,7 @@ const searchBox = document.getElementById(`searchBox`).children[1];
 const searchButton = document.getElementById(`searchBox`).children[2];
 const mainWindow = document.getElementById(`mainWindow`);
 const lowerWindow = document.getElementById(`lowerWindow`);
+const sidebar = document.getElementById(`cityList`);
 const apiKey = `048d27eceb416546d7a51110f55969be`;
 
 function getWeather(cityName, limit = 1) {
@@ -30,10 +31,19 @@ function getWeather(cityName, limit = 1) {
 };
 
 function addToSidebar(cityName) {
+    let cityButton = document.createElement(`button`);
+    cityButton.classList.add(`button`);
+    cityButton.classList.add(`is-fullwidth`);
+    cityButton.classList.add(`is-light`);
+    cityButton.type=`submit`;
+    cityButton.innerHTML=cityName;
+    cityButton.style.marginTop=`10px`;
+    sidebar.append(cityButton);
 }
 
 function fillWindows(cityName, weatherData) {
     weatherData = JSON.parse(weatherData);
+    addToSidebar(cityName);
     mainWindow.children[0].innerHTML = cityName + ` ` + weatherData[0].date;
     mainWindow.children[1].innerHTML = `Temp: ${weatherData[0].temp}`;
     mainWindow.children[2].innerHTML = `Wind: ${weatherData[0].wind}`;
@@ -47,6 +57,7 @@ function fillWindows(cityName, weatherData) {
     }
 }
 
+//search functionality
 searchButton.addEventListener(`click`, function (event) {
     event.preventDefault();
 
@@ -59,9 +70,22 @@ searchButton.addEventListener(`click`, function (event) {
         return;
     };
     if (localStorage.getItem(searchEntry)) {
-        console.log(`You've already searched that`);
+        for (let i = 0; i < sidebar.children.length; i++) {
+            if(sidebar.children[i].innerHTML==searchEntry){
+                sidebar.removeChild(sidebar.children[i]);
+            }
+        }
         fillWindows(searchEntry, localStorage.getItem(searchEntry));
         return;
     };
     getWeather(searchEntry);
+});
+
+//Click city name on sidebar to search
+sidebar.addEventListener(`click`, function(event){
+    event.preventDefault();
+    if(event.target.id==`cityList`) return;
+
+    sidebar.removeChild(event.target);
+    fillWindows(event.target.innerHTML,localStorage.getItem(event.target.innerHTML));
 })
